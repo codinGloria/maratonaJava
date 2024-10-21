f47078f241bbccab788fa28681305fe16e0ab321
codinGloria <gloriabraz.tech@gmail.com>
2024-10-21 16:54:35 -0300

    :books: Aula 14: Prática usando Tipos Primitivos

---
+++ Aula 14/TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package com.codingloria.maratonaJava.introducao;
+
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+
+        String nome = "Glória Braz";
+        String endereco = "Itaúna - MG";
+        double salario = 1550.54;
+        String dataRecebimento = "18/10/2024";
+        String relatorio = "Eu " + nome +
+                ", morando no endereço " + endereco +
+                ", confirmo que recebi o salário de " + salario +
+                ", na data " + dataRecebimento;
+
+        System.out.println(relatorio);
+    }
+}
