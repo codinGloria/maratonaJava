c8fbb4a3e29bfbce98dd525ff4f53a9f1b1f36d6
codinGloria <gloriabraz.tech@gmail.com>
2024-10-21 16:43:49 -0300

    :books: Aula 09: Comentários

---
+++ Aula 09/Comentario.java
@@ -0,0 +1,17 @@
+package com.codingloria.maratonaJava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class Comentario {
+    // isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de múltiplas
+        linhas
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo :]");
+    }
+}
